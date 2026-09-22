618c7c29c26d237f9f1076a728e8baec02c0a7ac
codeminesproject <santtoshupadhyay@gmail.com>
2026-09-22 10:22:30 +0530

    Lecture 22 09

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
-from dboperations import getSingleData,insert
+from dboperations import getSingleData,insert,getAllData
+from fastapi.responses import JSONResponse
 
 # create object of FastAPI
 obj=FastAPI()
@@ -32,10 +33,10 @@ def userLogin(request:LoginRequest):
                 "mobile":db_response[3],
                 "role":db_response[5],
                 }
-            return {"data":response}
+            return JSONResponse(status_code=200,content={"data":response})
         else:
-            return {"message":"Invalid Password"}
-    return {"data":"Invalid username"}
+            return JSONResponse(status_code=401,content={"message":"Invalid Password"})
+    return JSONResponse(status_code=401,content={"data":"Invalid username"})
 
 @obj.post("/register")
 def userRegistration(request:NewUserRequest):
@@ -43,23 +44,91 @@ def userRegistration(request:NewUserRequest):
     # check email exist
     email_status =  checkEmailExist(request.email)
     if email_status==True:
-        return {"message":"email already exist"}
+        return JSONResponse(status_code=400,content={"data":"email already exist"}) 
 
     # check mobile exist
     mobile_status =  checkMobileExist(request.mobile)
     if mobile_status==True:
-        return {"message":"mobile already exist"}
+        return JSONResponse(status_code=400,content={"data":"mobile already exist"})
 
     # check password match
     if request.password!=request.confirmed_password:
-        return {"message":"password and confirm password should be same"}
+        return JSONResponse(status_code=400,content={"data":"password and confirm password should be same"})
 
     query = f"INSERT INTO user_login (name, email, mobile, password, role) VALUES ('{request.name}', '{request.email}', '{request.mobile}', '{request.password}', '{request.role}')"
     db_response = insert(query)
     if db_response is not None:
-        return {"data":"User registered successfully"}
+        return JSONResponse(status_code=201,content={"data":"User registered successfully"}) 
 
-    return {"data":"something went wrong"}
+    return JSONResponse(status_code=500,content={"data":"something went wrong"}) 
+
+
+@obj.get("/get-all-users")
+def getAllUsers():
+    query = "select * from user_login"
+    db_response = getAllData(query)
+    if db_response is not None:
+        if len(db_response)>0:
+            users_list = []
+            for data in db_response:
+                response = {
+                                "id":data[0],
+                                "name":data[1],
+                                "email":data[2],
+                                "mobile":data[3],
+                                "role":data[5],
+                                }
+                users_list.append(response)
+            return JSONResponse(status_code=200,content={"data":users_list}) 
+        else:
+            return JSONResponse(status_code=404,content={"data":"no records found"})
+    else:
+        return JSONResponse(status_code=500,content={"data":"something went wrong"})
+
+@obj.get("/user-by-email")
+def getUsersByEmail(email):
+    query = f"select * from user_login where email='{email}'"
+    db_response = getSingleData(query)
+    if db_response is not None:
+        response = {
+                        "id":db_response[0],
+                        "name":db_response[1],
+                        "email":db_response[2],
+                        "mobile":db_response[3],
+                        "role":db_response[5],
+                        }
+        return JSONResponse(status_code=200,content={"data":response}) 
+    else:
+        return JSONResponse(status_code=404,content={"data":"no records found"}) 
+
+@obj.get("/access-account")
+def getUsersByEmail(email):
+    query = f"select * from user_login where email='{email}'"
+    db_response = getSingleData(query)
+    if db_response is not None:
+        if db_response[5]=="admin":
+            return JSONResponse(status_code=200,content={"data":"Account Report Shared"})
+        else:
+             return JSONResponse(status_code=403,content={"data":"Not allowed to access resources"})
+    else:
+        return JSONResponse(status_code=404,content={"data":"no records found"}) 
+
+@obj.get("/user-by-id/{id}")
+def getUsersById(id):
+    query = f"select * from user_login where id={id}"
+    db_response = getSingleData(query)
+    if db_response is not None:
+        response = {
+                        "id":db_response[0],
+                        "name":db_response[1],
+                        "email":db_response[2],
+                        "mobile":db_response[3],
+                        "role":db_response[5],
+                        }
+        return {"data":response}
+    else:
+        return {"data":"somethinf went wrong"}
+
 
 def checkEmailExist(email):
     query = f"select * from user_login where email='{email}'"
@@ -76,5 +145,9 @@ def checkMobileExist(mobile):
         return False
     else:
         return True 
+
+
+
+
 
 
